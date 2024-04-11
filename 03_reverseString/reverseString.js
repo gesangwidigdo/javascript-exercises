e6a012268e31db54d57436d53ec64e3067cea653
Gesang Widigdo <gesangwidigdo@gmail.com>
2024-04-12 06:33:59 +0700

    feat: solve exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let strLen = str.length;
+    let i = strLen - 1;
+    console.log(i);
+    console.log(str[i]);
+    let reversedString = ''
+    while (i >= 0) {
+        reversedString += str[i]
+        i--;
+    };
+    return reversedString;
 };
+
+// reverseString('hello')
 
 // Do not edit below this line
 module.exports = reverseString;
