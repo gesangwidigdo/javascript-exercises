b57cc39b00b08b41abb89061c9153a70f051df28
Gesang Widigdo <gesangwidigdo@gmail.com>
2024-04-12 06:20:17 +0700

    feat: solve exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let result = '';
+    if (num == 0) {
+        result = '';
+    } else if (num < 0) {
+        result = 'ERROR';
+    } else {
+        while(num--) {
+            result += str
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
