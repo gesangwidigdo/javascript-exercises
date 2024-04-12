b1bdd5a0a3dc436f4c05eae04b69c054e014c223
Gesang Widigdo <gesangwidigdo@gmail.com>
2024-04-12 08:03:09 +0700

    feat: solve exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removedItem) {
+    return array.filter((item) => !removedItem.includes(item));
 };
 
 // Do not edit below this line
