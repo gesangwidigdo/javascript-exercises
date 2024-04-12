aa48c68bb41540519e2f702806ae0cd28110d2f0
Gesang Widigdo <gesangwidigdo@gmail.com>
2024-04-12 08:28:32 +0700

    feat: solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (
+        (start < 0 || end < 0) ||
+        (!Number.isInteger(start) || !Number.isInteger(end))
+    ) {
+        return 'ERROR';
+    }
+    
+    let res = 0;
+    if (end >= start) {
+        for (let i = start; i <= end; i++) {
+            res += i;
+        }
+    } else if (start >= end) {
+        for (let i = start; i >= end; i--) {
+            res += i;
+        }
+    } else {
+        
+    }
+    return res;
 };
 
 // Do not edit below this line
