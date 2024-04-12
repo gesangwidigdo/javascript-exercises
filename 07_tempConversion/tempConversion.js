dfc7152e22a9a42db75a40b780b9accf745fac18
Gesang Widigdo <gesangwidigdo@gmail.com>
2024-04-12 09:24:06 +0700

    feat: solve all testcases in exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  return Math.round(((fahrenheitTemp - 32) * 5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  return Math.round(((celsiusTemp * (9/5)) + 32) * 10) / 10;
 };
+
+console.log(convertToFahrenheit(-10));
 
 // Do not edit below this line
 module.exports = {
